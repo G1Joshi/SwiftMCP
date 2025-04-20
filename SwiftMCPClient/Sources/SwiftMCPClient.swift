4056b0ed604bf1ff56c283bcf3593b56bb5049f1
Jeevan Joshi <joshijeevan98@gmail.com>
2025-04-20 20:19:14 +0530

    implement client

--- SwiftMCPClient/Sources/SwiftMCPClient.swift
+++ SwiftMCPClient/Sources/SwiftMCPClient.swift
@@ -10,5 +10,27 @@ import MCP
 
 @main
 struct SwiftMCPClient {
-    static func main() async throws {}
+    static func main() async throws {
+        print("Starting MCP...")
+
+        do {
+            let client = Client(
+                name: "SwiftMCPClient",
+                version: "0.1.0",
+                configuration: .default
+            )
+
+            let transport = StdioTransport()
+            try await client.connect(transport: transport)
+
+            let server = try await client.initialize()
+            print("Connected to Server: \(server.serverInfo.name) \(server.serverInfo.version)")
+
+            await client.disconnect()
+            print("Client Disconnected...")
+        } catch {
+            print("Error: \(error)")
+            exit(1)
+        }
+    }
 }
